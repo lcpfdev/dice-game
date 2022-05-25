e59f38a72e84833414b5d3e8da492372868255ac
Luka France <lukafrance@gmail.com>
2022-05-25 15:14:16 +0100

    first commit

--- app.js
+++ app.js
@@ -1,73 +1,86 @@
-document.getElementById("editNamesBtn").addEventListener('click', () => {
-    player1 = prompt("Change Player1 name");
-    player2 = prompt("Change Player2 name");
-
-    document.getElementById("player1")
-                    .innerHTML = player1;
-                      
-    document.getElementById("player2")
-                   .innerHTML = player2;
-})
+let player1TotalScore = 0
+let player2TotalScore = 0
 
 
-
-
-
-document.getElementById("rollBtn").addEventListener('click', () => {
-    
-    let randomThrow = randomNumbers()
-
-   let diceImageTag = document.getElementById("dicecover")
-
-   diceImageTag.src = 'dice'+randomThrow+'.png';
-   
-   let score = 0
-
-    score = randomThrow
-   
-   document.getElementById("score").innerHTML = score 
-    
-
-  
-
-   
- })
-
-
-    
-
-
-
- function randomNumbers () {
+function randomNumbers () {
     return Math.ceil(Math.random()*6);
 }
 
 
+document.getElementById("rollBtn1").addEventListener('click', () => {
+    
+    let randomThrow = randomNumbers()
 
+   let diceImageTag = document.getElementById("dicecover1")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   diceImageTag.src = 'dice'+randomThrow+'.png';
+   
+player1TotalScore += randomThrow;
 
   
+   document.getElementById("player1Score").innerHTML = player1TotalScore; 
 
 
 
+  if(player1TotalScore >= 20) {
+    alert("Player 1 is the winner!!!!")
+  } else {
+
+  }
+
+
+
+ })
+
+
+
+
+ 
+
+
+ document.getElementById("rollBtn2").addEventListener('click', () => {
+    
+     let randomThrow = randomNumbers()
+
+        let diceImageTag = document.getElementById("dicecover2")
+
+        diceImageTag.src = 'dice'+randomThrow+'.png';
+
+  player2TotalScore += randomThrow;
+   document.getElementById("player2Score").innerHTML = player2TotalScore 
+
+
+
+
+   if(player2TotalScore >= 20) {
+    alert("Player 2 is the winner!!!!")
+  } else {
+
+  }
+
+ 
+ })
+
+
+ document.getElementById("holdBtn").addEventListener('click', () => {
+ 
+      if(rollBtn1.style.visibility === 'visible') {
+        rollBtn1.style.visibility = 'hidden'
+      } else {
+        rollBtn1.style.visibility = 'visible'
+      }
+    
+    // rollBtn1.style.visibility = 'hidden';
+
+
+
+
+
+
+
+ })
+
+ 
+ 
+
+ 
